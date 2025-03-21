c40f03d87cb5e77b0ff0e0cd3f8f4a85985acdf9
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-21 20:51:24 +0300

    fetch from json and output the animals

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // Your code here
+const characterBar = document.getElementById("character-bar");
+
+const characterList = fetch("http://localhost:3000/characters")
+  .then((response) => response.json())
+  .then((characters) => {
+    characters.forEach((character) => {
+      const span = document.createElement("span");
+      span.innerText = character.name;
+      span.dataset.id = character.id;
+      characterBar.appendChild(span);
+    });
+  });
